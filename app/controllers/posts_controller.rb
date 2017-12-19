4b4962c01c30ea6b6fcc8310d20f77f3055a654a
github username <github email address>
2017-12-19 14:41:33 -0500

    done

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,9 +8,16 @@ class PostsController < ApplicationController
   end
 
   def update
-    @post.update(post_params)
+    @post.assign_attributes(post_params)
 
-    redirect_to post_path(@post)
+    if @post.valid?
+      @post.save
+      redirect_to post_path(@post)
+    else
+      flash[:error] = @post.errors.full_messages
+      render :edit
+    end
+
   end
 
   private
